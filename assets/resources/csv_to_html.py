1091da0979714f38a7bb8525a0dbd2e07b2e75bc
Lindsay Reynolds <78495351+ic17rey@users.noreply.github.com>
2021-06-01 23:09:57 -0400

    Cleanup of comparisons html

--- assets/resources/csv_to_html.py
+++ assets/resources/csv_to_html.py
@@ -1,2 +1,5 @@
+# First table had an extra index column not needed
+# Adding index=False so that it is not included in the table
+
 import pandas as pd
 pd.read_csv('cities.csv').to_html('../../visualizations/table.html', index=False)
